5381df292e0d4a42c53563d4df592a297362ed47
Dennis Mooibroek <dennis.mooibroek@rivm.nl>
2023-09-21 12:15:08 +0200

    Fixed a rare bug where percentiles cannot be calculated due to few observations

--- R/epa_percentile.R
+++ R/epa_percentile.R
@@ -14,15 +14,24 @@ epa_percentile <- function(x, prob, na.rm = TRUE) {
   x <- sort(x)
 
   if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
-    warning("argument is not numeric or logical: returning NA")
+    cli::cli_warn(c(
+      "{.var x} is not numeric or logical.",
+      "x" = "Returning {NA}."
+    ))
     return(NA_real_)
   }
   if (!is.numeric(prob) || length(prob) != 1L) {
-    stop("'prob' must be numeric of length one")
+    cli::cli_abort(c(
+      "{.var prob}  must be numeric of length one.",
+      "x" = "Please adjust your input."
+    ))
   }
 
   if (prob > 1) {
-    warning("prob should be between 0 - 1: returning NA")
+    cli::cli_warn(c(
+      "{.var prob} should have a value between 0 - 1.",
+      "x" = "Returning NA."
+    ))
     return(NA_real_)
   }
   if (na.rm) {
@@ -33,16 +42,33 @@ epa_percentile <- function(x, prob, na.rm = TRUE) {
 
   L <- ((n + 1) * prob)
   I <- as.integer(L)
-  F <- L - I
+  # if we have to few samples (18 or less for P05, I can become 0)
+  if (I == 0) {
+    I <- 1
+    F <- 0
+    cli::cli_warn(c(
+      "The length of the data (n={n}) is too small to calculate the requested percentile.",
+      "x" = "The minimum of the data is used instead."
+    ))
+  } else {
+    F <- L - I
+  }
 
   w1 <- 1 - F
   w2 <- F
   w3 <- 0
 
-  if ((n - I) >= 2) {
-    P <- w1 * x[I] + w2 * x[I + 1] + w3 * x[I + 2]
-  } else {
+  if ((n - I) <= 0) {
+    P <- x[n] # grab the maximum value as the best estimate
+    cli::cli_warn(c(
+      "The length of the data (n={n}) is too small to calculate the requested percentile.",
+      "x" = "The maximum of the data is used instead."
+    ))
+    
+  } else if ((n - I) == 1) {
     P <- w1 * x[I] + w2 * x[I + 1]
+  } else if ((n - I) >= 2) {
+    P <- w1 * x[I] + w2 * x[I + 1] + w3 * x[I + 2]
   }
   return(P)
 }
